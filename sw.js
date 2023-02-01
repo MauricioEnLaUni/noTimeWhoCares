9a0374610f4ae3d22d38d54ca33292e6b255fef0
MauricioEnLaUni <210804@utags.edu.mx>
2023-02-01 09:24:55 -0600

    asdas

--- sw.js
+++ sw.js
@@ -4,10 +4,16 @@ class ServiceWorker {
 
     const APP_SHELL = [
       "/",
-      "index.html",
-      "css/style.css",
-      "public/index.js",
-      "assets/icons/brachyIcon.svg"
+      "noTimeWhoCares/index.html",
+      "noTimeWhoCares/css/style.css",
+      "noTimeWhoCares/public/index.js",
+      "noTimeWhoCares/assets/icons/brachyIcon.png",
+      "noTimeWhoCares/assets/img/brachydios/mh3u/fmD73q.avif",
+      "noTimeWhoCares/assets/img/brachydios/mh4u/YRFwgT.avif",
+      "noTimeWhoCares/assets/img/brachydios/mhwi/WrJesY.avif",
+      "noTimeWhoCares/assets/img/logo/qBBV84.avif",
+      "noTimeWhoCares/assets/img/mugshot/50ZOpY.avif"
+
     ];
 
     self.addEventListener("install", (e) => {
